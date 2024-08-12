a9f1f8b7429ec98a0762cd946059bef2952d46e8
arcs_mr <murty.av@gmail.com>
2024-08-12 21:40:32 +0530

    complete backend

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+// Replace with your MongoDB connection string
+const mongoURI =
+   'mongodb+srv://arcs_mr:3j4J3ODJcqwpLxsH@blogpost.wk7xf.mongodb.net/BlogPost?retryWrites=true&w=majority&appName=BlogPost'
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const postSchema = new mongoose.Schema({
+   userId: Number,
+   title: String,
+   body: String,
+})
+
+const Post = mongoose.model('Post', postSchema, 'posts')
+
+app.get('/posts', async (req, res) => {
+   const posts = await Post.find()
+   res.json(posts)
+})
+
+app.post('/posts', async (req, res) => {
+   const newPost = new Post(req.body)
+   await newPost.save()
+   res.status(201).json(newPost)
+})
+
+app.put('/posts/:id', async (req, res) => {
+   const updatedPost = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+   })
+   res.json(updatedPost)
+})
+
+app.delete('/posts/:id', async (req, res) => {
+   await Post.findByIdAndDelete(req.params.id)
+   res.status(204).send()
+})
+
+const PORT = 3000
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`)
+})
